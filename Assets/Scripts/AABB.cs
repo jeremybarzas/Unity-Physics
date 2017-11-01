b0689af6a8882dad4e23f2aa509faea2b5501af7
Jeremy Barzas <jbarzas@gmail.com>
2017-11-01 17:48:17 -0500

    initial commit

---
+++ Assets/Scripts/AABB.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AABB
+{
+    public float xSize;
+    public float ySize;
+
+    public Vector2 position;
+    public Vector2 min;
+    public Vector2 max;
+
+    public AABB(float x, float y)
+    {
+        xSize = x;
+        ySize = y;
+    }
+
+    public void UpdatePosition(Vector3 pos)
+    {
+        position = new Vector2(pos.x, pos.y);
+
+        min.x = position.x - xSize;
+        max.x = position.x + xSize;
+
+        min.y = position.y - ySize;
+        max.y = position.y + ySize;
+    }
+
+    public bool TestOverlap(AABB a, AABB b)
+    {
+        // a on the left side of b
+        if (a.max.x > b.min.x && a.min.x < b.min.x)
+            if (a.max.y > b.min.y && a.min.y < b.min.y)
+                return true;
+
+        // a on the right side of b
+        if (a.max.x < b.min.x && a.min.x > b.min.x)
+            if (a.max.y < b.min.y && a.min.y > b.min.y)
+                return true;
+        
+        return false;
+    }
+}
---
+++ Assets/Scripts/CollisionObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CollisionObject : MonoBehaviour
+{
+    public AABB m_collider;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_collider = new AABB(5.0f, 5.0f);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        m_collider.UpdatePosition(transform.position);
+        m_collider.TestOverlap(m_collider, m_collider);
+    }
+}

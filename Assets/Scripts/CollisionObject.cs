025a447826050a1bfc61e36ad3bcdf98d1a942d3
Jeremy Barzas <jbarzas@gmail.com>
2017-11-01 17:56:06 -0500

    current progress

--- Assets/Scripts/CollisionObject.cs
+++ Assets/Scripts/CollisionObject.cs
@@ -6,17 +6,18 @@ using UnityEngine;
 public class CollisionObject : MonoBehaviour
 {
     public AABB m_collider;
+    public AABB otherCollider;
 
 	// Use this for initialization
 	void Start ()
     {
-        m_collider = new AABB(5.0f, 5.0f);
+        m_collider = new AABB(2.0f, 2.0f);
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
         m_collider.UpdatePosition(transform.position);
-        m_collider.TestOverlap(m_collider, m_collider);
+        m_collider.TestOverlap(m_collider, otherCollider);
     }
 }

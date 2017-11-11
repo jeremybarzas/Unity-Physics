ba08ae72d2f6b40922e686f26da0b57c1dbc2eb1
Jeremy Barzas <jbarzas@gmail.com>
2017-11-10 18:03:02 -0600

    current progress

--- Assets/Scripts/AgentFactory.cs
+++ Assets/Scripts/AgentFactory.cs
@@ -24,10 +24,10 @@ namespace Facehead
             {
                 var go = Instantiate(boidPrefab);
                 var skeleton = go.AddComponent<BoidBehaviour>();
-                var boid = ScriptableObject.CreateInstance<Boid>();
+                var boid = ScriptableObject.CreateInstance<Boid>();                
                 boid.Initialize();
+                skeleton.Set_Moveable(boid);
                 agents.Add(boid);
-                skeleton.Set_Moveable(boid);                
             }
         }
 

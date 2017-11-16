0cce798b2c6aa5b8446ce5e995595a8b05b5fdab
Jeremy Barzas <jbarzas@gmail.com>
2017-11-16 10:29:19 -0600

    Sliders are working correctly.

---
+++ Assets/Scripts/SliderBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderBehaviour : MonoBehaviour
+{
+    public Slider sliderComponent;
+    public Text textComponent;
+    
+    // Unity methods
+    void Start ()
+    {
+        sliderComponent = GetComponent<Slider>();
+        textComponent = GetComponentInChildren<Text>();
+        Set_Text();
+    }	
+	
+	void Update ()
+    {
+        Set_Text();
+    }
+
+    // methods
+    private void Set_Text()
+    {
+        textComponent.text = textComponent.name + ": " + ((int)sliderComponent.value).ToString();
+    }
+}
